60a69cb21ef773db918d85fae43edd70f91d0f3c
21120297 <21120297@student.hcmus.edu.vn>
2023-10-24 12:10:17 +0700

    fix util read file

--- graphic/util.py
+++ graphic/util.py
@@ -8,15 +8,15 @@ def readMap(filename):
             size = tuple(map(int, f.readline().split()))
 
             # Read the map data and convert to a list of lists
-            map = [list(map(int, line.split())) for line in f]
+            map_data = [list(map(int, line.split())) for line in f]
 
             # Extract the position from the last row
-            pos = map.pop()
-            return size, map, pos
+            pos = map_data.pop()
+            return size, map_data, pos
     except FileNotFoundError:
         print("Could not open file")
     except Exception as e:
-        print("Error reading Map")
+        print(e)
         
-path = f"../test/lv1_map1.txt"
-readMap(path)
+#path = "2023_HCMUS_AI_P01/test/lv1_map1.txt"
+#readMap(path)
